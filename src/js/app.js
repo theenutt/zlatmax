a0f6f319a939ed89e7379491eb271a1404ea1ede
theenutt <you@example.com5107624@gmail.com>
2022-12-05 00:06:42 +0300

    catalog-header css

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,19 @@
+$(document).ready(function() {
+    $('.phones-header__arrow').click(function(event){
+        $(this).toggleClass('active').next().slideToggle(300);
+    });
+});
+
+$('.popup__open').click(function() {
+    $('.popup').fadeIn(600);
+    $('html').addClass('no-scroll');
+});
+
+$('.popup__close').click(function() {
+    $('.popup').fadeOut(600);
+    $('html').removeClass('no-scroll');
+});
+
 import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
@@ -5,3 +21,4 @@ flsFunctions.isWebp();
 import Swiper, { Navigation, Pagination } from 'swiper';
 
 const swiper = new Swiper();
+
