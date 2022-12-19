608ebd7e7b3e8e3c90a205c69467da500f4f3b74
theenutt <you@example.com5107624@gmail.com>
2022-12-19 03:11:26 +0300

    add sub-menu js 3.46

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
     });
 });
 
+//======================================================================================
+
 $('.popup__open').click(function() {
     $('.popup').fadeIn(600);
     $('html').addClass('no-scroll');
@@ -13,6 +15,34 @@ $('.popup__close').click(function() {
     $('.popup').fadeOut(600);
     $('html').removeClass('no-scroll');
 });
+
+//=======================================================================================
+
+document.addEventListener("click", documentActions);
+
+function documentActions(e) {
+    const targetElement = e.target;
+    if (targetElement.closest('[data-parent]')) {
+        const subMenuId = targetElement.dataset.parent ? targetElement.dataset.parent : null;
+        const subMenu = document.querySelector(`[data-submenu="${subMenuId}"]`);
+        const catalogMenu = document.querySelector('.catalog-header');
+        if (subMenu) {
+            const activeLink = document.querySelector('._sub-menu-active');
+            const activeBlock = document.querySelector('._sub-menu-open');
+            if (activeLink && activeLink !== targetElement) {
+                activeLink.classList.remove('_sub-menu-active');
+                activeBlock.classList.remove('_sub-menu-open');
+            }
+            targetElement.classList.toggle('_sub-menu-active');
+            subMenu.classList.toggle('_sub-menu-open');
+    } else {
+        console.log('нет такого подменю')
+    }
+    e.preventDefault();
+ }
+}
+
+//===============================================
 
 import * as flsFunctions from "./modules/functions.js";
 
